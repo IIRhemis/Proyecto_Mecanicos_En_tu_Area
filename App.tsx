e506e054c5860e1fbcac08af4ac3a78952e4975e
Valentina Sepulveda <valentina.sepulveda@innobec.cl>
2022-12-03 12:41:28 -0300

    Se hace creacion del mapa

--- App.tsx
+++ App.tsx
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { MapScreen } from './src/screens/MapScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>K pazaaaaaaa ijos del piko</Text>
-      <StatusBar style="auto" />
-    </View>
+    // <View style={styles.container}>
+    //   <Text>K pazaaaaaaa ijos del piko</Text>
+    //   <StatusBar style="auto" />
+    // </View>
+    <MapScreen/>
   );
 }
 
