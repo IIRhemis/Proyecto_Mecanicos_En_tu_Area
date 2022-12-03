635ad97df3173e7f2a3ef27b1d51d8c889947299
IIRhemis <119139790+IIRhemis@users.noreply.github.com>
2022-12-03 11:14:39 -0400

    Cambios Login y carpetas
    Ajustes al login y se cambio de lugar el login

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { InicioScreen } from './src/components/screens/InicioScreen';
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>K pazaaaaaaa ijos del piko</Text>
-      <StatusBar style="auto" />
+      {/* <Text>K pazaaaaaaa ijos del piko</Text>
+      <StatusBar style="auto" /> */}
+      <InicioScreen/>
     </View>
   );
 }
