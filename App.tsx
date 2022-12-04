aa61ff5618d0bca9f433d0adac5ad28f50406dd5
Paloma <paloma.velasquezm@utem.cl>
2022-12-03 22:18:18 -0300

    Actualización de desarrollo

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ContainerExample } from './src/screens/ScreenPrincipal';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>K pazaaaaaaa ijos del piko</Text>
-      <StatusBar style="auto" />
+      <ContainerExample/>
+      <StatusBar style="auto"/>
     </View>
   );
 }
---
+++ src/screens/ScreenPrincipal.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import Image from 'react-bootstrap/Image';
+import { StyleSheet, Text, View } from 'react-native';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+
+export function ContainerExample () {
+    return (
+        <View style={styles.container}>
+            <Container>
+                <Row>
+                    <Col>
+                    <Text>1 of 1</Text>
+                    </Col>
+                </Row>
+            </Container>
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
