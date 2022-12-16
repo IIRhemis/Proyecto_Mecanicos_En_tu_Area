75c61b45ae4893c7d07b9f0ee21cd2d7660e8bbd
Paloma <paloma.velasquezm@utem.cl>
2022-12-16 01:43:20 -0300

    Actualización de botones

--- src/screens/ScreenPrincipal.tsx
+++ src/screens/ScreenPrincipal.tsx
@@ -1,27 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import CSS from 'csstype';
 import React from 'react';
-import { StyleSheet, Text, View, Image, Button} from 'react-native';
+import { StyleSheet, Text, View, Image, Button,SafeAreaView} from 'react-native';
 
-function ContainerExample(){
-    const handleClick = () => {
-        // Add code to handle the click event here
-    };
-    
-    return (
-        <View style={styles.container}>
+const Separator = () => (
+    <View style={styles.separator} />
+);
+
+const handleClick = () => {
+    // Add code to handle the click event here
+};
+
+const ContainerExample = () =>(
+    <SafeAreaView style={styles.container}>
+        <View >
                 <Image
-                    style={{ width: 350, height: 200, marginBottom: 15 }}
+                    style={{ width: 360, height: 200, margin:15, }}
                     source={require("./assets/mecanico1.png")}
                 />
-                <View style={styles.btnContainer}>
-                <Button color={'#A22C29'} onClick={handleClick} title="Ver Mecanicos"></Button>
-                <Button color={'#A22C29'} onClick={handleClick} title="Hacer Consulta"></Button>
-                <Button color={'#A22C29'} onClick={handleClick} title="¿Estás solo? Encuentra Mecánicos en tu área!"></Button>
-                </View>    
-            </View>
-    );
-}
+        </View>
+        <View >
+                <Button 
+                    title="Ver Mecanicos"
+                    color={'#A22C29'} 
+                    onPress={handleClick} 
+                />
+                <Separator></Separator>
+                <Button 
+                    color={'#A22C29'} 
+                    onPress={handleClick}  
+                    title="Hacer Consulta"
+                />
+                <Separator></Separator>
+                <Button 
+                    color={'#A22C29'} 
+                    onPress={handleClick}  
+                    title="¿Estás solo? Encuentra Mecánicos en tu área!"
+                />  
+            
+        </View>
+    </SafeAreaView>
+);
 
 export default ContainerExample;
 
@@ -32,11 +51,17 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         backgroundColor: '#D6D5C9',
     },
-    imageContainer: {
-        flex: 1,
-    paddingTop: 210,
-    },
     btnContainer:{
         padding:20,
+    },
+    separator: {
+        marginVertical: 8,
+        borderBottomColor: '#D6D5C9',
+        borderBottomWidth: StyleSheet.hairlineWidth,
+    },
+    fixToText: {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginBottom: 15,
     },
 });
