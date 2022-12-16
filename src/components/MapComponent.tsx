a094e7af96c8c48bbf906ec9870f8591ff0ae5ce
Valentina Sepulveda <valentina.sepulveda@innobec.cl>
2022-12-15 21:56:37 -0300

    Se terminan configuraciones del mapa

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+import * as Location from 'expo-location'
 import MapView,  {Marker, Polyline} from 'react-native-maps';
 import { StyleSheet, View } from 'react-native';
 import MapViewDirections from 'react-native-maps-directions';
+import credentials from '../../credentials';
+
 
 export const MapComponent = () =>{
 
@@ -14,6 +17,33 @@ export const MapComponent = () =>{
       longitud: -70.658539
     })
 
+    async function getLocationPermission(){
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if(status !== 'granted'){
+        alert('permiso denegado')
+        return
+      }
+      let location = await Location.getCurrentPositionAsync({});
+      const current ={
+        latitud: location.coords.latitude,
+        longitud: location.coords.longitude
+      }
+      SetOrigin(current);
+    }
+
+    async function getLocation(){
+      let location = await Location.getCurrentPositionAsync({});
+      const current ={
+        latitud: location.coords.latitude,
+        longitud: location.coords.longitude
+      }
+      SetOrigin(current);
+    }
+
+    React.useEffect(()=>{
+      getLocationPermission();
+    }, [])
+
     return (
         <View style={styles.container}>
           <MapView style={styles.map} 
@@ -23,26 +53,30 @@ export const MapComponent = () =>{
             latitudeDelta: 0.09,
             longitudeDelta: 0.04
           }}
+          showsMyLocationButton= {true}
+          zoomControlEnabled = {true}
           >
 
           <Marker 
             coordinate={{latitude: Origin.latitud,longitude: Origin.longitud}}
+            
             // onDragEnd = {(direction) => SetOrigin(direction.nativeEvent.coordinate)}
           />
           <Marker 
             coordinate={{latitude: Destination.latitud, longitude: Destination.longitud}}
-            
           />
-          <Polyline
+          {/* <Polyline
             coordinates = {[{latitude: Origin.latitud,longitude: Origin.longitud},{latitude: Destination.latitud, longitude: Destination.longitud}]}
 
-          />
-
-          {/* <MapViewDirections
-            origin={Origin}
-            destination={Destination}
-            apikey={}
           /> */}
+
+          <MapViewDirections
+            origin={{latitude: Origin.latitud,longitude: Origin.longitud}}
+            destination={{latitude: Destination.latitud, longitude: Destination.longitud}}
+            apikey={credentials.GOOGLE_MAP_KEY}
+            strokeColor = "black"
+            strokeWidth={6}
+          />
 
           </MapView>
         </View>
