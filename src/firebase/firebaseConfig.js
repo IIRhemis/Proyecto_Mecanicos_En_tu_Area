e7f5c155d92400ac0c4f9cf150a4dfe37a500927
Valentina Sepulveda <valentina.sepulveda@innobec.cl>
2022-12-16 19:20:10 -0300

    configuracion firebase

---
+++ src/firebase/firebaseConfig.js
@@ -0,0 +1,16 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBAUK1yAH3pNcSnpiQRR61oWKsVKWIZVlU",
+  authDomain: "computacion-movil1.firebaseapp.com",
+  projectId: "computacion-movil1",
+  storageBucket: "computacion-movil1.appspot.com",
+  messagingSenderId: "7450810782",
+  appId: "1:7450810782:web:aeaec7f010916aa9a8de8f"
+};
+
+// Initialize Firebase
