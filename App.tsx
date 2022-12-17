646d4d9cb9609e8de80f6a249633201a741ed13a
Paloma <paloma.velasquezm@utem.cl>
2022-12-16 21:53:45 -0300

    Actualizacion

--- App.tsx
+++ App.tsx
@@ -1,15 +1,25 @@
-import { StatusBar } from 'expo-status-bar';
-import {  View, Text, Image, StyleSheet } from 'react-native';
+import * as React from 'react';
+import { Button, StyleSheet } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
 import  ContainerExample  from './src/screens/ScreenPrincipal';
+import InicioScreen from './src/screens/InicioScreen';
+
+const Stack = createNativeStackNavigator();
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <ContainerExample/>
-      <StatusBar style="auto"/>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ContainerExample} />
+        <Stack.Screen name="Inicio" component={InicioScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
+
 
 const styles = StyleSheet.create({
   container: {
